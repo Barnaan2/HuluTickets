124731808be81409c13b1f94e4e0d4582ee925f8
amu <Amansis.g2@gmail.com>
2022-04-26 17:08:36 +0300

    a

--- resources/views/layouts/Base.blade.php
+++ resources/views/layouts/Base.blade.php
@@ -17,7 +17,7 @@
 </head>
     <div class="header-dark">
         <nav class="navbar navbar-dark  navbar-expand-lg navigation-clean-search">
-            <div class="container"><a class="navbar-brand" href="#">Abc</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle
+            <div class="container"><a class="navbar-brand" href="#"><img class="navbar-brand" src="assets/img/logowhite.png"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle
                         navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav">
@@ -41,7 +41,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-6 col-md-3 item">
-                            <h3>movies</h3>
+                            <h3>Hulu movies</h3>
                             <ul>
                                 <li><a href="#">Drama</a></li>
                                 <li><a href="#">comedy</a></li>
@@ -57,7 +57,8 @@
                             </ul>
                         </div>
                         <div class="col-md-6 item text">
-                            <h3>abc</h3>
+                            <h3>Hulu Tickets
+                            </h3>
                             <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
                         </div>
                         <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i
@@ -65,7 +66,7 @@
                                     class="icon ion-social-snapchat"></i></a><a href="#"><i
                                     class="icon ion-social-instagram"></i></a></div>
                     </div>
-                    <p class="copyright">abc © 2022</p>
+                    <p class="copyright">Hulu Tickets © 2022</p>
                 </div>
             </footer>
         </div>
